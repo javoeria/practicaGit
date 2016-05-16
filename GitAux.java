7884f08e93a61c9f5648d64baf611f9ce7df3028
javier-rb <javi.malaga12@gmail.com>
2016-05-16 17:43:18 +0200

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -10,16 +10,20 @@ package prgitejemplo;
 public class GitAux {
     
     public void metodoAlumno1(){
-	// Comentario incluido para pruebas
         System.out.println("metodo 1");
     }
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	// modificaciones alumno 2
+	System.out.println("modificado por alumno 2"); // alumno 2
     }
     
     public void metodoComunitario(){
+	// Comentario incluido para pruebas
         System.out.println("Aqui escribimos todos");
+	// modificaciones alumno 2
+	System.out.println("Alumno 2 inserta esta sentencia");
     }
     
     public void testGitAux(){
